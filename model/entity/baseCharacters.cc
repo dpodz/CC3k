8c488530d0636e88a2c5222e22209db5e0cbd927
dipodlov <dipodlov@student.cs.uwaterloo.ca>
2016-11-29 00:16:45 -0500

    Adding all subclasses for characters

--- model/entity/baseCharacters.cc
+++ model/entity/baseCharacters.cc
@@ -8,17 +8,74 @@
 using namespace std;
 
 Shade::Shade(): Character{1, Stats{25, 25, 0, 1.0}, 125, 125} { }
-
 Shade::~Shade() { }
 
 int Shade::getScore() const {
 	return getGold() * 1.5;
 }
 
-void Shade::lookedOnBy(shared_ptr<Character> character) {
-	// TODO
+//VAMPIRE
+Vampire::Vampire(): Character{1, Stats{25, 25, 0, 1.0}, 50, 50} { }
+Vampire::~Vampire() { }
+
+//TODO
+void attack(std::shared_ptr<Entity>) { }
+
+//DROW
+Drow::Drow(): Character{1, Stats{25, 15, 0, 1.5	}, 150, 150} { }
+Drow::~Drow() { }
+
+//GOBLIN
+Goblin::Goblin(): Character{1, Stats{15, 20, 0, 1.0}, 110, 110} { }
+Goblin::~Goblin() { }
+
+//TODO
+void Goblin::onKill() { }
+
+//TROLL
+Troll::Troll(): Character{1, Stats{25, 15, 5, 1.0}, 120, 120} { }
+Troll::~Troll() { }
+
+//DEFAULT ENEMY CHARACTERS
+//HUMAN
+Human::Human(): Character{2, Stats{20, 20, 0, 1.0}, 140, 140} { }
+Human::~Human() { }
+
+vector<shared_ptr<Entity>> Human::onDeath() {
+	vector<shared_ptr<Entity>> dropOnDeath;
+	for (int i = 0 ; i < 2 ; i++) {	dropOnDeath.emplace_back(new NormalGold); } //add 2 normal gold piles
+	return dropOnDeath;
 }
 
-void Shade::attack(shared_ptr<Entity> entity) {
-	// TODO
-}
+//Dwarf
+Dwarf::Dwarf(): Character{2, Stats{20, 30, 0, 1.0}, 100, 100}  { }
+Dwarf::~Dwarf() { }
+
+//Halfling
+Halfling::Halfling(): Character{2, Stats{15, 20, 0, 1.0}, 100, 100} { }
+Halfling::~Halfling() { }
+
+//Elf
+Elf::Elf(): Character{2, Stats{30, 10, 0, 1.0}, 140, 140} { }
+Elf::~Elf() { }
+
+//TODO
+void Elf::attack(shared_ptr<Character> character) { }
+
+//Orc
+Orc::Orc(): Character{2, Stats{30, 25, 0, 1.0}, 180, 180} { }
+Orc::~Orc() { }
+
+//TODO
+void Orc::attack(shared_ptr<Character>) { }
+
+//Dragon
+Dragon::Dragon(): Character{3, Stats{20, 20, 0, 1.0}, 150, 150} { }
+Dragon::~Dragon() { }
+
+//Merchant
+Merchant::Merchant(): Character{4, Stats{70, 5, 0, 1.0}, 30, 30} { }
+Merchant::~Merchant() { }
+
+//TODO
+vector<shared_ptr<Entity>> Merchant::onDeath() {}
--- model/entity/baseCharacters.h
+++ model/entity/baseCharacters.h
@@ -4,15 +4,95 @@
 #include "character.h"
 
 class Shade : public Character {
-
 public:
 	Shade();
 	~Shade();
 
 	int getScore() const override;
-	void lookedOnBy(std::shared_ptr<Character>) override;
-	void attack(std::shared_ptr<Entity>) override;
-
 };
 
+//Vampire
+class Vampire : public Character {
+public:
+	Vampire();
+	~Vampire();
+	
+	void attack(std::shared_ptr<Entity>) override;
+};
+
+//Drow
+class Drow : public Character {
+public:
+	Drow();
+	~Drow();
+};
+
+//Goblin
+class Goblin : public Character {
+public:
+	Goblin();
+	~Goblin();
+	void onKill() override;
+};
+
+//Troll
+class Troll : public Character {
+public:
+	Troll();
+	~Troll();
+};
+
+//Human
+class Human : public Character {
+public:
+	Human();
+	~Human();
+	std::vector<std::shared_ptr<Entity>> onDeath() override;
+};
+
+//Dwarf
+class Dwarf : public Character {
+public:
+	Dwarf();
+	~Dwarf();
+};
+
+//Halfling
+class Halfling : public Character {
+public:
+	Halfling();
+	~Halfling();
+	void getAttackedBy(std::shared_ptr<Character>) override;
+};
+
+//Elf
+class Elf : public Character {
+public:
+	Elf();
+	~Elf();
+	void attack(std::shared_ptr<Character>) override;
+};
+
+//Orc
+class Orc: public Character {
+public:
+	Orc();
+	~Orc();
+	void attack(std::shared_ptr<Character>) override;
+};
+
+//Dragon
+class Dragon: public Character {
+	std::shared_ptr<DragonHoard> treasure;
+public:
+	Dragon();
+	~Dragon();
+};
+
+//Merchant
+class Merchant: public Character {
+public:
+	Merchant();
+	~Merchant();
+};
 #endif // BASE_CHARACTERS_H
