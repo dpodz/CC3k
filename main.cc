624eee24d51bec2a9fc1e2903a562a09c644ffa0
Alex Tomala <actomala@uwaterloo.ca>
2016-12-04 12:29:36 -0500

    Added proper main.cc stuff

--- main.cc
+++ main.cc
@@ -1,18 +1,33 @@
 #include <iostream>
-#include "messaging/messages.h"
-#include "messaging/observer.h"
-#include "messaging/subject.h"
 #include "view/CLIView.h"
 #include "controller/CLIController.h"
 #include <memory>
+#include <string>
 
 using namespace std;
 
-int main() {
- 	
-	string mapString = "maps/FastWin.map";
+int main(int argc, char* argv[]) {
 
-	auto controller = make_shared<CLIController>(mapString, true);
+	string mapString = "default.map";
+	bool customMap = false;
+
+	if (argc > 3) {
+		cerr << "Invalid number of arguments entered" << endl;
+		return 1;
+	}
+
+	if (argc > 1) {
+		if (string(argv[1]) == "--custom") {
+			if (argc < 3) {
+				cerr << "Map file not specified" << endl;
+				return 1;
+			}
+			mapString = argv[2];
+			customMap = true;
+		}
+	}
+
+	auto controller = make_shared<CLIController>(mapString, customMap);
 
 	controller->playGame();
 
