56af1a28651f44b6011b835234375142a420c8fe
Alex Tomala <actomala@uwaterloo.ca>
2016-12-05 14:03:54 -0500

    Docs mostly

--- main.cc
+++ main.cc
@@ -3,10 +3,14 @@
 #include "controller/CLIController.h"
 #include <memory>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
+
+	srand(time(NULL));
 
 	string mapString = "default.map";
 	bool customMap = false;
